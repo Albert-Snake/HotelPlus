8cd984ff91cc2dca7fc96f063d51fed223672bb4
Rafael Guerra <2201145@my.ipleiria.pt>
2022-11-16 02:25:43 +0000

    Alterações Frontend Index

--- website/frontend/views/layouts/main.php
+++ website/frontend/views/layouts/main.php
@@ -35,13 +35,15 @@ AppAsset::register($this);
         ],
     ]);
     $menuItems = [
-        ['label' => 'Home', 'url' => ['/site/index']],
-        ['label' => 'About', 'url' => ['/site/about']],
-        ['label' => 'Contact', 'url' => ['/site/contact']],
+//        ['label' => 'Home', 'url' => ['/site/index']],
+        ['label' => 'Quartos', 'url' => ['/site/about']],
+        ['label' => 'Marcações', 'url' => ['/site/about']],
+        ['label' => 'Sobre', 'url' => ['/site/about']],
+        ['label' => 'Contactos', 'url' => ['/site/contact']],
     ];
-    if (Yii::$app->user->isGuest) {
-        $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
-    }
+//    if (Yii::$app->user->isGuest) {
+//        $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
+//    }
 
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav me-auto mb-2 mb-md-0'],
@@ -49,6 +51,7 @@ AppAsset::register($this);
     ]);
     if (Yii::$app->user->isGuest) {
         echo Html::tag('div',Html::a('Login',['/site/login'],['class' => ['btn btn-primary login text-decoration-none']]),['class' => ['d-flex']]);
+        echo Html::tag('div',Html::a('Signup',['/site/signup'],['class' => ['btn btn-primary login text-decoration-none']]),['class' => ['d-flex']]);
     } else {
         echo Html::beginForm(['/site/logout'], 'post', ['class' => 'd-flex'])
             . Html::submitButton(
