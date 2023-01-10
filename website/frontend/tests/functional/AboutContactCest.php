fd707ce3101b3d423544bd6a2c97af160b1f051c
Simao <simaoventura01@gmail.com>
2023-01-10 19:07:25 +0000

    Ultimo teste funcional

---
+++ website/frontend/tests/functional/AboutContactCest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace frontend\tests\functional;
+
+use frontend\tests\FunctionalTester;
+
+class AboutContactsCest
+{
+    public function _before(FunctionalTester $I)
+    {
+        
+    }
+
+    // tests
+    public function tryToTest(FunctionalTester $I)
+    {
+        $I->amOnPage(\Yii::$app->homeUrl);
+        $I->click('Iniciar Sessão');
+        $I->fillField('Username', 'Alberto');
+        $I->fillField('Password', '123456789');
+        $I->click('login-button');
+        $I->click('Sobre');
+        $I->click('Contactos');
+        
+        
+    }
+}
