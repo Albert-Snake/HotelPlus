6b421c73e6e8fd4c9c5342d68a3ac09744cdea6c
Simao <simaoventura01@gmail.com>
2023-01-10 16:25:36 +0000

    Alteração no teste funcional de signup e elminação de testes
    alterar uma cena no teste de signup e eliminar os testes que tinham sido criados automaticamente

--- website/frontend/tests/functional/SignupCest.php
+++ website/frontend/tests/functional/SignupCest.php
@@ -17,11 +17,12 @@ class SignupCest
     // tests
     public function tryToTest(FunctionalTester $I)
     {
-        $I->amOnRoute('/site/signup');
+        $I->amOnRoute(\Yii::$app->homeUrl);
+        $I->click('Registar');
         $I->fillField('SignupForm[username]','teste');
         $I->fillField('SignupForm[email]','teste@gmail.pt');
         $I->fillField('SignupForm[nome]','Teste');
-        $I->fillField('SignupForm[apelido]','teste@gmail.pt');
+        $I->fillField('SignupForm[apelido]','Testes');
         $I->fillField('SignupForm[nif]',919191919);
         $I->fillField('SignupForm[telefone]',987654321);        
         $I->fillField('SignupForm[password]','password_teste');
